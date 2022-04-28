8fead6e7977856eeb254dc721a9525c25231f81f
anniprovietti <anniprovietti@gmail.com>
2022-04-28 12:48:19 -0300

    Create exer3.py
    operações matemáticas

---
+++ exer3.py
@@ -0,0 +1,8 @@
+#Operações matemáticas no python
+
+print("Soma 344 + 455 =", 344+455);
+print("Subtração 344 - 455 =", 344-455);
+print("Multiplicação 3*4*4 =", 3*4*4);
+print("Divisão 344/4 =", 344/4);
+print("Porcentagem 10%344=", 10%344);
+print("Comparação 344 > 455?", 344>455);
