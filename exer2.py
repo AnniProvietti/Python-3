ff91cc894f84812eae1b367c8d3af6da876a9dbd
anniprovietti <anniprovietti@gmail.com>
2022-04-28 12:48:04 -0300

    Create exer2.py
    comentário

---
+++ exer2.py
@@ -0,0 +1,8 @@
+#Segundo exercicio de python com print
+
+print("I could have code like this.") #imprime na tela
+
+#Linha comentada não imprime nada
+#print("This won't run.")
+
+print("This will run.");
