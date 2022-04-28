8258c27afe61800d556796300106283c03f0b8c6
anniprovietti <anniprovietti@gmail.com>
2022-04-28 14:48:12 -0300

    Create exer5.py
    string de formatação

---
+++ exer5.py
@@ -0,0 +1,10 @@
+nome = "Anni"
+idade = 23
+idioma = 3
+carros_estoque_inicial = 34
+carros_vendidos = 12 
+carros_estoque_final = carros_estoque_inicial - carros_vendidos
+
+print(f"Meu nome é {nome},tenho {idade}anos e falo {idioma} idiomas.")
+print(f"Minha loja tem atualmente {carros_estoque_final} carros no estoque.")
+print(f"{nome} {idade} {carros_estoque_inicial} {carros_vendidos} {carros_estoque_final}")
