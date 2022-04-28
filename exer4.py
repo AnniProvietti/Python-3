82c6ed426f5fe8dab658d9112804c80386d2067b
anniprovietti <anniprovietti@gmail.com>
2022-04-28 14:41:41 -0300

    Create exer4.py
    variáveis e nomes

---
+++ exer4.py
@@ -0,0 +1,10 @@
+
+nome = "Anni"
+idade = 23
+idioma = 3
+carros_estoque_inicial = 34
+carros_vendidos = 12 
+carros_estoque_final = carros_estoque_inicial - carros_vendidos
+
+print("Meu nome é", nome,"tenho", idade,"anos e falo",idioma, "idiomas.")
+print("Minha loja tem atualmente", carros_estoque_final, "carros no estoque.")
